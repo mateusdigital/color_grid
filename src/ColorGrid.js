06ceebfb837ae79f95ff18adbddd09b547e53195
stdmatt <stdmatt@pixelwizards.io>
2019-08-29 00:12:30 +0300

    Add the check for game over conditions.

--- src/ColorGrid.js
+++ src/ColorGrid.js
@@ -36,8 +36,8 @@ function Setup()
     let color_count = 4;
     let width  = Canvas_Width - 20;
     let height = width;
-    let rows = 20;
-    let cols = 20;
+    let rows = 5;
+    let cols = 5;
 
     palette       = new Palette(color_count);
     board         = new Board(-width/2, -height/2, width, height, rows, cols, color_count);
