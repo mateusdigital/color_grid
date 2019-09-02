4f87417c832fba892961ae3d2d28a1d072960450
stdmatt <stdmatt@pixelwizards.io>
2019-09-02 18:50:42 +0300

    Finish the basic implementation.

--- src/OptionsPanel.js
+++ src/OptionsPanel.js
@@ -19,6 +19,11 @@ class OptionsPanel
         this.animationTimer = new Timer(0.3);
         this.isAnimating    = false;
         this.isVisible      = false;
+
+        // State
+        this.desiredRows   = -1;
+        this.desiredCols   = -1;
+        this.desiredColors = -1;
     } // ctor
 
     //--------------------------------------------------------------------------
@@ -29,6 +34,9 @@ class OptionsPanel
 
         this.animationTimer.start();
         this.isAnimating = true;
+
+        this._updateState();
+        this._updateUI   ();
     } // show
 
     //--------------------------------------------------------------------------
@@ -86,6 +94,8 @@ class OptionsPanel
             this.sizeLabel  .draw();
 
             this.saveButton.draw();
+            Canvas_SetFillStyle("White");
+            this.saveLabel.draw();
 
             Canvas_Push();
                 Canvas_Translate(0, 80);
@@ -112,15 +122,24 @@ class OptionsPanel
     _buildColorSliderUI()
     {
         this.colorsSlider = new UISlider(
-            "red", "gray", "white",
+            chroma("red"),   chroma("gray"),
+            chroma("white"), chroma("white").darken(2),
             Vector_Create(0, Canvas_Edge_Top + 50),
             Vector_Create(200, 5),
             (s, value) => {
-                let count = Math_Int(Math_Map(value, 0, 1, 3, 5));
-                let str   = String_Cat("Colors: ", count);
+                let count = Math_Int(Math_Map(
+                    value,
+                    0, 1,
+                    GAME_OPTIONS_MIN_COLORS,
+                    GAME_OPTIONS_MAX_COLORS
+                ));
+
+                // Update the Text.
+                let str = String_Cat("Colors: ", count);
                 this.colorsLabel.setString(str);
 
-                gameOptions.colorsCount = count;
+                // Save the value.
+                this.desiredColors = count;
             }
         );
         this.colorsLabel = new UIText(
@@ -137,16 +156,25 @@ class OptionsPanel
     _buildSizeSliderUI()
     {
         this.sizeSlider = new UISlider(
-            "red", "gray", "white",
+            chroma("red"),   chroma("gray"),
+            chroma("white"), chroma("white").darken(2),
             Vector_Create(0, this.colorsLabel.position.y + this.colorsLabel.height + 30),
             Vector_Create(200, 5),
             (s, value) => {
-                let count = Math_Int(Math_Map(value, 0, 1, 4, 20));
-                let str   = String_Cat("Colors: ", count);
+                let count = Math_Int(Math_Map(
+                    value,
+                    0, 1,
+                    GAME_OPTIONS_MIN_ROWS,
+                    GAME_OPTIONS_MAX_ROWS
+                ));
+
+                // Update the Text.
+                let str = String_Cat("Size: ", count);
                 this.sizeLabel.setString(str);
 
-                gameOptions.gridRows   = value;
-                gameOptions.gridCols   = value;
+                // Save the value.
+                this.desiredCols = count;
+                this.desiredRows = count;
             }
         );
         this.sizeLabel = new UIText(
@@ -176,9 +204,21 @@ class OptionsPanel
             Vector_Create(-100, -60),
             Vector_Create(200, 50),
             ()=>{
+                gameOptions.applyOptions(
+                    this.desiredColors,
+                    this.desiredRows,
+                    this.desiredCols,
+                );
                 ResetGame();
                 this.hide();
             }
+        );
+
+        this.saveLabel = new UIText(
+            "SAVE",
+            Vector_Create(0, -39),
+            20,
+            "arial"
         );
     } // _buildSaveUI
 
@@ -202,4 +242,31 @@ class OptionsPanel
         this.texts.push(t);
     } // _buildCreditsUI
 
+    _updateState()
+    {
+        this.desiredRows   = gameOptions.gridCols;
+        this.desiredCols   = gameOptions.gridCols;
+        this.desiredColors = gameOptions.colorsCount;
+
+        Log(this.desiredRows, this.desiredCols, this.desiredColors);
+    }
+
+    _updateUI()
+    {
+        let sv = Math_Map(
+            this.desiredRows,
+            GAME_OPTIONS_MIN_ROWS,
+            GAME_OPTIONS_MAX_ROWS,
+            0, 1
+        );
+        this.sizeSlider.updateValue(sv);
+
+        let cv = Math_Map(
+            this.desiredColors,
+            GAME_OPTIONS_MIN_COLORS,
+            GAME_OPTIONS_MAX_COLORS,
+            0, 1
+        );
+        this.colorsSlider.updateValue(cv);
+    }
 }; // class OptionsPanel
