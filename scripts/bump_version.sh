be1ddfcb34d283cfe0a3bdbe70df9adf263b97ba
stdmatt <stdmatt@pixelwizards.io>
2020-03-25 00:08:31 +0300

    Add the bump-the-version to start to handle the versioning

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -6,15 +6,14 @@
 ##                   \__ \ || (_| | | | | | | (_| | |_| |_                    ##
 ##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
 ##                                                                            ##
-##  File      : build.sh                                                      ##
+##  File      : bump_version.sh                                               ##
 ##  Project   : color_grid                                                    ##
-##  Date      : Mar 14, 2020                                                  ##
+##  Date      : Mar 24, 2020                                                  ##
 ##  License   : GPLv3                                                         ##
 ##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
 ##  Copyright : stdmatt 2020                                                  ##
 ##                                                                            ##
 ##  Description :                                                             ##
-##                                                                            ##
 ##---------------------------------------------------------------------------~##
 
 ##----------------------------------------------------------------------------##
@@ -26,52 +25,17 @@ source /usr/local/src/stdmatt/shellscript_utils/main.sh
 ##----------------------------------------------------------------------------##
 ## Variables                                                                  ##
 ##----------------------------------------------------------------------------##
-PROJECT_NAME="color grid";
-PROJECT_PACKAGE_NAME="color_grid";
-
 SCRIPT_DIR="$(pw_get_script_dir)";
-ROOT_DIR="$(pw_abspath "${SCRIPT_DIR}/..")";
-BUILD_DIR="${ROOT_DIR}/build";
-DIST_DIR="${ROOT_DIR}/dist";
-TO_COPY="libs/ src/ res/ css/ img/ index.html";
+SRC_DIR="$(pw_abspath "${SCRIPT_DIR}/../src")";
+SRC_FILE="${SRC_DIR}/Version.js";
 
 
 ##----------------------------------------------------------------------------##
 ## Script                                                                     ##
 ##----------------------------------------------------------------------------##
-#
-# Build
-echo "Cleaning build directory";
-rm    -rf "$BUILD_DIR";
-mkdir -p  "$BUILD_DIR";
+BUMP_THE_VERSION="$(pw_get_program_path "bump-the-version")";
+if [ -z "$BUMP_THE_VERSION" ]; then
+    pw_log_fatal "Coundn't find (bump-the-version) program - Aborting...";
+fi;
 
-
-echo "Copying files to build directory";
-for ITEM in $TO_COPY; do
-    cp -R "${ROOT_DIR}/${ITEM}" "${BUILD_DIR}";
-done;
-
-find ${BUILD_DIR} -iname ".git*" -exec rm -rf {} \;
-
-
-##
-## Dist
-test -z "$(pw_getopt_exists "--dist" "$@")" && exit;
-echo "Creating the distribution artifact!!!";
-
-FINAL_VERSION=$(bump-the-version  \
-    "${ROOT_DIR}/src/Version.js"     \
-    "const COLOR_GRID_VERSION"       \
-    show                             \
-);
-
-echo "Cleaning dist directory";
-rm    -rf "$DIST_DIR";
-mkdir -p  "$DIST_DIR";
-
-echo "Creating zip file";
-ZIP_FILENAME="${PROJECT_PACKAGE_NAME}_v${FINAL_VERSION}.zip";
-cd ${BUILD_DIR}
-zip -r "${ZIP_FILENAME}" ".";
-mv "${ZIP_FILENAME}" ${DIST_DIR};
-cd -
+"${BUMP_THE_VERSION}" "${SRC_FILE}" "const COLOR_GRID_VERSION =" bump "$1";
--- src/Version.js
+++ src/Version.js
@@ -14,12 +14,11 @@
 //                                                                            //
 //  Description :                                                             //
 //---------------------------------------------------------------------------~//
+
 //----------------------------------------------------------------------------//
 // Constants                                                                  //
 //----------------------------------------------------------------------------//
-const COLOR_GRID_VERSION_MAJOR = 1;
-const COLOR_GRID_VERSION_MINOR = 0;
-const COLOR_GRID_VERSION_BABY  = 0;
+const COLOR_GRID_VERSION = "1.0.1";
 
 //----------------------------------------------------------------------------//
 // Public Functions                                                           //
@@ -27,9 +26,5 @@ const COLOR_GRID_VERSION_BABY  = 0;
 //------------------------------------------------------------------------------
 function GetVersion()
 {
-    return String_Cat(
-        COLOR_GRID_VERSION_MAJOR, ".",
-        COLOR_GRID_VERSION_MINOR, ".",
-        COLOR_GRID_VERSION_BABY
-    );
+    return COLOR_GRID_VERSION;
 }
